a6230665ac6e98aae791c82f3c4a3a23303cd671
Carkzis <marc.jowett@gmail.com>
2021-02-20 15:31:07 +0000

    Tidied colour list queue.

--- cadmus.py
+++ cadmus.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from collections import deque
 
 # Create main window and title it.
 window = tk.Tk()
@@ -13,12 +14,12 @@ frm_colours.grid(row=0, column=0)
 # Create a list for the colour labels and grids
 rgb = []
 # Create dictionary so correct colour is applied at the correct part of loop
-rgb_text = {0: "Red:", 2: "Green:", 4: "Blue:"}
+rgb_text = deque(["Red", "Green", "Blue"])
 
 # Loop for putting the colour labels and grids into the frame
-for i in range(0, 6):
+for i in range(0, len(rgb_text)*2):
     if i == 0 or i % 2 == 0:
-        rgb.append(tk.Label(master=frm_colours, text=rgb_text[i]))
+        rgb.append(tk.Label(master=frm_colours, text=rgb_text.popleft()))
         rgb[i].grid(row=0, column=i, sticky="e")
     else:
         rgb.append(tk.Entry(master=frm_colours, width=5))
